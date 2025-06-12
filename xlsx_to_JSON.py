715bd81e9f9788d31bb2dca7401845cfbd416e06
Weslley <103947523+WeslleySebastiao@users.noreply.github.com>
2025-06-12 09:07:16 -0300

    Delete xlsx_to_JSON.py

--- xlsx_to_JSON.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Ler o arquivo XLSX
-xlsx_file_path = 'C:/Users/eaws00155707/Desktop/Agente/Pasta1.xlsx'
-df = pd.read_excel(xlsx_file_path)
-
-# Converter o DataFrame para JSON
-json_file_path = 'C:/Users/eaws00155707/Desktop/Agente/JSON/ods+iniciativas.JSON'
-df.to_json(json_file_path, orient='records', force_ascii=False, indent=4)
